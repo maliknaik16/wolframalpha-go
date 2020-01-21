f55cd4e89e0f67f9b1b4824014918f6a535c9270
Malik Naik <maliknaik16@gmail.com>
2020-01-22 02:24:30 +0530

    Add languagemsg.go

---
+++ languagemsg.go
@@ -0,0 +1,29 @@
+
+package wolfram
+
+import (
+  "encoding/xml"
+)
+
+// Struct for the <languagemsg> tag.
+type LanguageMsg struct {
+  XMLName             xml.Name                `xml:"languagemsg,omitempty"`
+  English             string                  `xml:"english,attr"`
+  Other               string                  `xml:"other,attr"`
+}
+
+// The interface for the LanguageMsg.
+type ILanguageMsg interface {
+  GetEnglish()        string
+  GetOther()          string
+}
+
+// Returns the "english" attribute value from <languagemsg>.
+func (l *LanguageMsg) GetEnglish() string {
+  return l.English
+}
+
+// Returns the "other" attribute value from <languagemsg>.
+func (l *LanguageMsg) GetOther() string {
+  return l.Other
+}
