6bc7cb5b7acd281584bbd868d2da1287c0048957
Malik Naik <maliknaik16@gmail.com>
2020-01-21 15:38:05 +0530

    Add didyoumean.go

---
+++ didyoumean.go
@@ -0,0 +1,36 @@
+
+package wolfram
+
+import (
+	"encoding/xml"
+)
+
+// Struct for the <didyoumean> tag.
+type DidYouMean struct {
+  XMLName             xml.Name                `xml:"didyoumean,omitempty"`
+  Score               string                  `xml:"score,attr"`
+  Level               string                  `xml:"level,attr"`
+  Value               string                  `xml:",chardata"`
+}
+
+// The interface for the DidYouMean
+type IDidYouMean interface {
+	GetScore()					string
+	GetLevel()					string
+	GetValue()					string
+}
+
+// Returns the "score" attribute value from the <didyoumean>.
+func (dym *DidYouMean) GetScore() string {
+	return dym.Score
+}
+
+// Returns the "level" attribute value from the <didyoumean>.
+func (dym *DidYouMean) GetLevel() string {
+	return dym.Level
+}
+
+// Returns the value of the <didyoumean>.
+func (dym *DidYouMean) GetValue() string {
+	return dym.Value
+}
