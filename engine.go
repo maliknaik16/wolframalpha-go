b6e6f9b64765c2e5184dcb29210648cced35c6eb
Malik Naik <maliknaik16@gmail.com>
2020-01-31 18:15:59 +0530

    Add engine.go

---
+++ engine.go
@@ -0,0 +1,14 @@
+
+package wolfram
+
+type Engine struct {
+	server 		string
+	path 			string
+	appId			string
+}
+
+func NewEngine(appId string) *QueryResult {
+	queryResult := &QueryResult{}
+
+	return queryResult
+}
